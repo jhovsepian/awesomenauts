31ca763998625243890a7350d535152ee20e1c53
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2015-01-30 09:09:02 -0800

    made player jump higher

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -45,7 +45,7 @@ game.PlayerEntity = me.Entity.extend ({
 			this.body.vel.x = 0;
 		}
 		// makes the player jump and fall back down
-		if(me.input.isKeyPressed("jump") && !this.jumping && !this.falling) {
+		if(me.input.isKeyPressed("jump") && !this.body.jumping && !this.body.falling) {
 			this.jumping = true;
 			this.body.vel.y -= this.body.accel.y * me.timer.tick;
 		}
