d14c2f5f8451a6774f7fb323715dca7ae47e224a
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2015-02-05 21:41:55 -0800

    video 18 enemy creep class

--- js/game.js
+++ js/game.js
@@ -43,6 +43,7 @@ var game = {
 			me.pool.register("player", game.PlayerEntity, true);
 			me.pool.register("PlayerBase", game.PlayerBaseEntity);
 			me.pool.register("EnemyBase", game.EnemyBaseEntity);
+			me.pool.register("EnemyCreep", game.EnemyCreep, true);
 			// 2 screens
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
