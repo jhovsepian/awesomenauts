871d2dced7a62de135d08baed8701b63ee45cd96
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2015-02-13 14:56:16 -0800

    vid 21

--- js/entities/entities.js
+++ js/entities/entities.js
@@ -12,6 +12,7 @@ game.PlayerEntity = me.Entity.extend ({
 				return(new me.Rect(0, 0, 64, 64)).toPolygon();
 			}
 		}]);
+		this.type = "PlayerEntity";
 		// chooses velocity for our player
 		this.body.setVelocity(5, 20);
 		// keeps track of which direction your character is going
@@ -92,6 +93,11 @@ game.PlayerEntity = me.Entity.extend ({
 		this._super(me.Entity, "update", [delta]);
 		return true;
 	},
+
+	loseHealth: function(damage) {
+		this.health = this.health - damage;
+	},
+
 	// info about the handler
 	collideHandler: function(response) {
 		// to see whatever were colliding with
@@ -296,6 +302,24 @@ game.EnemyCreep = me.Entity.extend ({
 			this.pos.x = this.pos.x + 1;
 			//checks that it has been at least 1 second since this creep hit a base
 			if((this.now-this.lastHit >= 1000)) {
+				//updates the lasthit timer
+				this.lastHit = this.now;
+				//makes the player base call its losehealth function and passes it a
+				//damage of 1
+				response.b.loseHealth(1);
+			}
+		}else if(response.b.type==='PlayerEntity') {
+			var xdif = this.pos.x - response.b.pos.x;
+
+			this.attacking=true;
+			//this.lastAttacking=this.now;
+			this.body.vel.x = 0;
+			//keeps moving the creep to the right to maintain its position
+			if(xdif>0) {			
+				this.pos.x = this.pos.x + 1;
+		}	
+			//checks that it has been at least 1 second since this creep hit something
+			if((this.now-this.lastHit >= 1000) && xdif>0) {
 				//updates the lasthit timer
 				this.lastHit = this.now;
 				//makes the player base call its losehealth function and passes it a
