c9a192c0731b995405b1dce1404c71e2a9706798
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2015-03-06 09:18:37 -0800

    video 40 continue

--- js/screens/title.js
+++ js/screens/title.js
@@ -33,6 +33,29 @@ game.TitleScreen = me.ScreenObject.extend({
 			}
 		})));
 
+		// this is for the text and font of the title screen to show
+		me.game.world.addChild(new (me.Renderable.extend({
+			init: function() {
+				// call to the super class
+				this._super(me.Renderable, 'init', [380, 340, 250, 50]);
+				// my font
+				this.font = new me.Font("Chiller", 70, "white");
+				me.input.registerPointerEvent('pointerdown', this, this.newGame.bind(this), true);
+			},
+			// to try and draw on the screen
+			draw: function(renderer) {
+				this.font.draw(renderer.getContext(), "Play Now to Continue", this.pos.x, this.pos.y);
+			},
+
+			update: function(dt) {
+				return true;
+			},
+
+			newGame: function() {
+				me.input.releasePointerEvent('pointerdown', this);
+				me.state.change(me.state.PLAY);
+			}
+		})));
 
 	},
 	
