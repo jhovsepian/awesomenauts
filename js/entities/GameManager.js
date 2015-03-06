ea8e0bd4554f8fe3ece1b5d248d3111048937e52
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2015-03-06 09:47:31 -0800

    video 41 tested out some code

--- js/entities/GameManager.js
+++ js/entities/GameManager.js
@@ -50,15 +50,15 @@ game.HeroDeathManager = Object.extend({
 game.ExperienceManager = Object.extend({
 	init: function(x, y, settings) {
 		this.alwaysUpdate = true;
-		this.gameOver = false;
+		this.gameover = false;
 	},
 
 	update: function() {
 		// wont work till flags are set
-		if(game.data.win === true && !this.gameOver) {
+		if(game.data.win === true && !this.gameover) {
 			this.gameOver(true);
 			// wont be called until flags are set
-		}else if(game.data.win === false && !this.gameOver) {
+		}else if(game.data.win === false && !this.gameover) {
 			// if i lose gains only 1 
 			this.gameOver(false);
 		}
@@ -74,7 +74,8 @@ game.ExperienceManager = Object.extend({
 			game.data.exp += 1;
 		}
 
-		this.gameOver = true;
+		this.gameover = true;
 		me.save.exp = game.data.exp;
+		
 	}
 });
--- js/game.js
+++ js/game.js
@@ -6,8 +6,8 @@ var game = {
 	data : {
 		// score
 		score : 0,
-		enemyBaseHealth: 10,
-		playerBaseHealth: 10,
+		enemyBaseHealth: 1,
+		playerBaseHealth: 1,
 		enemyCreepHealth: 10,
 		playerHealth: 10,
 		enemyCreepAttack: 1,
@@ -50,6 +50,7 @@ var game = {
 	}
 
 	me.save.add({exp: 0, exp1: 0, exp2: 0, exp3: 0, exp4: 0});
+
 
 	// Initialize the audio.
 	me.audio.init("mp3,ogg");
