ce63552e08532f70eb647d13f5b30ce90076dbef
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2015-03-29 22:09:10 -0700

    video 52 added new and load screens

--- index.php
+++ index.php
@@ -36,10 +36,11 @@
 		<script type="text/javascript" src="js/gamemanagers/HeroDeathManager.js"></script>
 		<script type="text/javascript" src="js/entities/EnemyCreep.js"></script>
 		<script type="text/javascript" src="js/entities/HUD.js"></script>
-
 		<script type="text/javascript" src="js/screens/title.js"></script>
 		<script type="text/javascript" src="js/screens/play.js"></script>
 		<script type="text/javascript" src="js/screens/spendExp.js"></script>
+		<script type="text/javascript" src="js/screens/loadProfile.js"></script>
+		<script type="text/javascript" src="js/screens/newProfile.js"></script>
 		<!-- /build -->
 		<!-- Bootstrap & Mobile optimization tricks -->
 		<script type="text/javascript">
---
+++ js/screens/loadProfile.js
@@ -0,0 +1,39 @@
+game.LoadProfile = me.ScreenObject.extend({
+	/**	
+	 *  action to perform on state change
+	 */
+	onResetEvent: function() {	
+		me.game.world.addChild(new me.Sprite(0, 0, me.loader.getImage('load-screen')), -10); // TODO
+		
+		me.input.unbindKey(me.input.KEY.B);
+		me.input.unbindKey(me.input.KEY.Q);
+		me.input.unbindKey(me.input.KEY.E);
+		me.input.unbindKey(me.input.KEY.W);
+		me.input.unbindKey(me.input.KEY.A);
+		
+
+		// this is for the text and font of the title screen to show
+		me.game.world.addChild(new (me.Renderable.extend({
+			init: function() {
+				// call to the super class
+				this._super(me.Renderable, 'init', [10, 10, 300, 50]);
+				// my font
+				this.font = new me.Font("Chiller", 50, "white");
+			
+			},
+			// to try and draw on the screen
+			draw: function(renderer) {
+				this.font.draw(renderer.getContext(), "ENTER YOUR USERNAME AND PASSWORD", this.pos.x, this.pos.y);
+			}
+		})));
+
+	},
+	
+	
+	/**	
+	 *  action to perform when leaving this screen (state change)
+	 */
+	onDestroyEvent: function() {
+
+	}
+});
