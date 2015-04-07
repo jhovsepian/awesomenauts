5df8cfcc02d393444781f3bca20100c0786bcaa8
Jacqueline Hovsepian <jhovsepian1018@gmail.com>
2015-04-06 17:51:15 -0700

    video I5  setting up minimap files intermediate

--- index.php
+++ index.php
@@ -62,6 +62,7 @@
 		<script type="text/javascript" src="js/screens/title.js"></script>
 		<script type="text/javascript" src="js/screens/play.js"></script>
 		<script type="text/javascript" src="js/screens/SpearThrow.js"></script>
+		<script type="text/javascript" src="js/screens/MiniMap.js"></script>
 		<script type="text/javascript" src="js/screens/spendExp.js"></script>
 		<script type="text/javascript" src="js/screens/loadProfile.js"></script>
 		<script type="text/javascript" src="js/screens/newProfile.js"></script>
--- js/game.js
+++ js/game.js
@@ -41,7 +41,8 @@ var game = {
 		win: "",
 		pausePos: "",
 		buyscreen: "",
-		buytext: ""
+		buytext: "",
+		minimap: ""
 
 	},
 	
@@ -89,7 +90,8 @@ var game = {
 			me.pool.register("HeroDeathManager", game.HeroDeathManager);
 			me.pool.register("ExperienceManager", game.ExperienceManager);
 			me.pool.register("SpendGold", game.SpendGold);
-			me.pool.register("spear", game.SpearThrow);
+			me.pool.register("spear", game.SpearThrow, true);
+			me.pool.register("minimap", game.MiniMap, true);
 			// 2 screens
 		me.state.set(me.state.MENU, new game.TitleScreen());
 		me.state.set(me.state.PLAY, new game.PlayScreen());
